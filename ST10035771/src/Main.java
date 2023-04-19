95be966b422c285fe8fbfeba3ee5d535a089804b
Enzo35771 <st10035771@vcconnect.edu.za>
2023-04-19 21:04:00 +0200

    first commit

--- ST10035771/src/Main.java
+++ ST10035771/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
        String surname = JOptionPane.showInputDialog(null, "What is your surname?");
        String username = JOptionPane.showInputDialog(null, "Enter your username:");
        String password = JOptionPane.showInputDialog(null, "Enter your password:");
-
+//object
        Login user = new Login(username, password);
        user.checkUserName(username);
        user.checkPasswordComplexity(password);
